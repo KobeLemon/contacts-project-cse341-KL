8ea490fbd678e5606d5f33c76ee3e11a6b4532c6
KobeLemon <kobe42lemon@outlook.com>
2023-09-17 23:25:17 -0600

    Added getAll and getSingle

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+const mongodb = require('./data/database');
+
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port3000 = process.env.PORT || 3000;
 
 app.use('/', require('./routes'))
 
-app.listen(port, () => console.log(`Running on port ${port}`));
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port3000, () => console.log(`Database is listening & Node is running on port ${port3000}`));
+    }
+})
+
